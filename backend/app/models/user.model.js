da28911c8eeb01071013a00b49b555dbe5dbed11
navilindo <hassainjr@gmail.com>
2024-02-18 23:40:24 +0300

    user model

---
+++ backend/app/models/user.model.js
@@ -0,0 +1,33 @@
+module.exports = (mongoose, mongoosePaginate) => {
+    var schema = mongoose.Schema(
+      {
+        username: {type: String, unique: true, required: true },
+        email: {type: String, unique: true, required: true },
+        password: {type: String, required: true },
+        first_name: String,
+        last_name: String,
+        nickname: String,
+        gender: String,
+        phone: String,
+        roles: [
+          {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Role"
+          }
+        ]
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    schema.plugin(mongoosePaginate);
+    const findOrCreate = require('mongoose-findorcreate')
+    schema.plugin(findOrCreate);
+  
+    return mongoose.model("User", schema);
+  }
