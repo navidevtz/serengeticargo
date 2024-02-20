364a1a502c0884b588a0c705b3533977d437f1d9
navilindo <hassainjr@gmail.com>
2024-02-20 13:00:13 +0300

    template

---
+++ frontend/src/components/layout/MainContainer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main">
+        <!-- Navigation -->
+        <Navbar :topbarColor="topbarColor"></Navbar>
+        <div class="main-container" :class="{ 'sidebar-active': isSidebarActive}">
+            <div>
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
--- frontend/src/components/layout/navbar.vue
+++ frontend/src/components/layout/navbar.vue
@@ -5,6 +5,9 @@
             <li class="nav-item">
                 <router-link to="/track" class="nav-link">Track</router-link>
             </li>
+            <li class="nav-item">
+                <router-link to="/" class="nav-link">Track</router-link>
+            </li>
         </div>
     </nav>
 </template>
