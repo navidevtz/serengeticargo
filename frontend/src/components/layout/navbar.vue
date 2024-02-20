22c1255b280b210664cb224f01ac78d8b039d3c0
navilindo <hassainjr@gmail.com>
2024-02-20 12:19:03 +0300

    track link

--- frontend/src/components/layout/navbar.vue
+++ frontend/src/components/layout/navbar.vue
@@ -1,5 +1,10 @@
 <template>
     <nav class="navbar">
         <router-link to="/" class="navbar-brand">Serengeti Cargo</router-link>
+        <div class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <router-link to="/track" class="nav-link">Track</router-link>
+            </li>
+        </div>
     </nav>
 </template>
