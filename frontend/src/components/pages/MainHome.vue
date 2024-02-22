cf2cc60f4a77ad34143abc0095fac236d250d97a
navilindo <hassainjr@gmail.com>
2024-02-22 23:21:33 +0300

    home renamed to main home

---
+++ frontend/src/components/pages/MainHome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="">Home page</div>
+</template>
+
+<script>
+export default {
+    name: 'MainHome',
+    title: 'Home',
+}
+</script>
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -9,27 +9,7 @@ export default new Router ({
     routes: [
         {
             path: "/",
-            component: Home,
-        },
-        {
-            path: "/users",
-            name: "users",
-            component: () => import("./components/user/Users")
-        },
-        {
-            path: "/users/:id",
-            name: "/user-details",
-            component: () => import("./components/user/User")
-        },
-        {
-            path: "/users/:id",
-            name: "/edit-user",
-            component: () => import("./components/user/EditUser")
-        },
-        {
-            path: "/users/new",
-            name: "/add-user",
-            component: () => import("./components/user/AddUser")
+            component: () => import("../components/pages/MainHome")
         }
     ]
 });
