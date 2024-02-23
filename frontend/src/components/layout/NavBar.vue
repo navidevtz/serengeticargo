625e6c77b7254af1767f1f4fbf4891db54a40793
navilindo <hassainjr@gmail.com>
2024-02-23 19:43:21 +0300

    bootstrap

--- frontend/src/components/layout/NavBar.vue
+++ frontend/src/components/layout/NavBar.vue
@@ -1,14 +1,24 @@
 <template>
+    <!-- <<nav
+        class="nav justify-content-center  "
+    >
+        <a class="nav-link active" href="#" aria-current="page"
+            >Active link</a
+        >
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link disabled" href="#">Disabled link</a>
+    </nav> -->
+    
     <nav class="navbar">
         <div class="container">
             <router-link to="/" class="navbar-brand">Serengeti Cargo</router-link>
             <div class="navbar-nav mr-auto">
-                <li class="nav-item">
+                <a class="nav-item">
                     <router-link to="/track" class="nav-link">Track</router-link>
-                </li>
-                <li class="nav-item">
+                </a>
+                <a class="nav-item">
                     <router-link to="/users/add" class="nav-link">Register</router-link>
-                </li>
+                </a>
             </div>
         </div>
     </nav>
