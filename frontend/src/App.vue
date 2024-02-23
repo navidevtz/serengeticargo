f74b4c29ea8b75e1cba9eb89a2cf447912fc25ed
navilindo <hassainjr@gmail.com>
2024-02-23 12:02:12 +0300

    navbar renamed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,7 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import MainHome from './components/pages/MainHome.vue'
-import NavBar from "./components/layout/NavBar.vue"
+import NavBar from './components/layout/Navbar'
 
 export default {
   name: 'App',
