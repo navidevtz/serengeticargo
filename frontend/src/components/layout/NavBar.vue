36a5cfeed5e3e95d71642d52c1c01a5a205bad06
navilindo <hassainjr@gmail.com>
2024-02-23 12:12:20 +0300

    title

--- frontend/src/components/layout/NavBar.vue
+++ frontend/src/components/layout/NavBar.vue
@@ -1,13 +1,16 @@
 <template>
     <nav class="navbar">
-        <router-link to="/" class="navbar-brand">Serengeti Cargo</router-link>
-        <div class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <router-link to="/track" class="nav-link">Track</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/users/add" class="nav-link">Register</router-link>
-            </li>
+        <div class="container">
+            <router-link to="/" class="navbar-brand">Serengeti Cargo</router-link>
+            <div class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <router-link to="/track" class="nav-link">Track</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/users/add" class="nav-link">Register</router-link>
+                </li>
+            </div>
         </div>
+        
     </nav>
 </template>
