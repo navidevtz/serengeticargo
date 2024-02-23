8dcbfcb89d7b0cc335cc6e30053eb778a415961b
navilindo <hassainjr@gmail.com>
2024-02-23 11:46:01 +0300

    serengeti logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/serengeticargo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <MainHome karibu="Karibu Serengeti Cargo"/>
   </div>
 </template>
