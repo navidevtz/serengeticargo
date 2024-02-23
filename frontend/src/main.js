4d28733d0936d8b259668deec63b8f32d7886389
navilindo <hassainjr@gmail.com>
2024-02-23 19:52:50 +0300

    bootstrap imported

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 Vue.config.productionTip = false
 
